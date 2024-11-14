7b3c557e0991d350b842a1f26f02250831ada2c6
elanore <elanorejoseph@gmail.com>
2024-11-14 23:43:31 +0530

    class object in java

--- Demo.java
+++ Demo.java
@@ -1,23 +1,45 @@
-class Calculator {
-    int a;
+class Computer {
+    // void specify no return
+    public void playMusic() {
+        System.out.println("Music is playing ");
 
-    // can be called from anywhere -public
-    // return value type
-    public int add(int n1, int n2) {
-        int r = n1 + n2;
-        // System.out.println("in add");
-        return r;
+    }
+
+    public String getMeaPen(int cost) {
+        if (cost >= 10)
+            return "Pen";
+        else
+            return "nothing";
+
+        // System.out.println("Pen");
     }
 }
 
+// class Calculator {
+// int a;
+
+// can be called from anywhere -public
+// add is method
+// public int add(int n1, int n2) {
+// int r = n1 + n2;
+// System.out.println("in add");
+// return r;
+// }
+// }
+
 public class Demo {
     public static void main(String a[]) {
-        int num1 = 4;
-        int num2 = 3;
+        // int num1 = 4;
+        // int num2 = 3;
         // class name variable name = new key word
-        Calculator calc = new Calculator();
-        int result = calc.add(num1, num2);
-        System.out.println(result);
+        // Calculator calc = new Calculator();
+        // int result = calc.add(num1, num2);
+        // System.out.println(result);
+        // obj reference variable
+        Computer obj = new Computer();
+        obj.playMusic();
+        String str = obj.getMeaPen(12);
+        System.out.println(str);
 
     }
 
